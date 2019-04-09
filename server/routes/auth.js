357c4a475b99a5e078454d418df26f81944a01d7
mridul1809 <mridulgupta11944@gmail.com>
2019-04-10 02:24:18 +0530

    Signup Validation

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -7,38 +7,43 @@ const router = express.Router();
 
 router.post('/signup' , function (req , res , next) {
   passport.authenticate('signup', function(err , user , info) {
+
     if(err) {
-      console.log(err.errmsg);
-      return next(err);
+      
+      if(err.code == 11000) {
+        res.statusCode = 409;
+        return res.json( { error : "Username or Email already exists!"});
+      }
+      else
+        return next(err);
     }
-    
-    return res.redirect(307 , '/auth/login');
+    else
+      return res.redirect(307 , '/auth/login');
 
   })(req , res , next);
 });
 
 router.post('/login', async (req, res, next) => {
-    passport.authenticate('login', async (err, user, info) => {     try {
-        if(err || !user){
-          const error = new Error('An Error occurred')
+    passport.authenticate('login', async (err, user, info) => {     
+        
+        if(!user)
+          return res.json( {error : 'Invalid username or password!'});
 
-          return next(error);
-        }
-        req.login(user, { session : false }, async (error) => {
-          if( error ) return next(error)
-          //We don't want to store the sensitive information such as the
-          //user password in the token so we pick only the email and id
-          //Sign the JWT token and populate the payload with the user id
-          let token = jwt.sign({
-            userId : user._id ,
-            exp : Math.floor(Date.now() / 1000) + (60),
-          },process.env.EXPRESS_SECRET);
+        else if(err)
+          return next(err);
+
+        else {
+
+          let token = jwt.sign( { userId : user._id} ,process.env.EXPRESS_SECRET , { expiresIn: '1h' });
           //Send back the token to the user
           token = 'Bearer ' + token;
-          return res.json({ token });
-        });     } catch (error) {
-        return next(error);
-      }
+          return res.json( {
+            data : {
+              user : user._id
+            },
+            token : token
+          })
+        }
     })(req, res, next);
   });
   
